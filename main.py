e6a3c271ab019f8e8bc5ce5408fa86117a902956
huskycgn <jjslappn!8>
2023-10-05 17:50:19 +0200

    corrected string to be sent.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 from datetime import datetime
 
 # List of tracking numbers - just paste yours.
-tr_numberlist = [TRACKING_NUMBER]
+tr_numberlist = TRACKING_NUMBER
 
 tr = trackerclass.Tracker()
 
@@ -15,22 +15,24 @@ def send_message(message):
     bot.send_message(chat_id=CHAT_ID, text=message)
 
 
-curr_str = [ ]
+curr_str = []
 
 for n in tr_numberlist:
-    curr_str.append((n, tr.getstatus(n)[ 'status' ]))
+    curr_str.append((n, tr.getstatus(n)['status']))
+
 # print(curr_str)
 
 while True:
     try:
         for i, n in enumerate(tr_numberlist):
-            print(curr_str[i])
-            new_str = n, tr.getstatus(n)[ 'status' ]
-            print(new_str)
+            # print(curr_str[i])
+            new_str = n, tr.getstatus(n)['status']
             print(datetime.now())
+            print(new_str)
             if curr_str[i] != new_str:
                 curr_str[i] = new_str
-                send_message(new_str)
+                msg_string = str(f'{new_str[0][0]}:\n{new_str[0][1]}')
+                send_message(msg_string)
     except ConnectionError:
         continue
     sleep(360)
